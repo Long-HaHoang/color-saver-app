db1ddc3bc8175909daae1f4ade5c41a773559618
LongHaHoang <longhahoang1992@gmail.com>
2022-12-22 23:51:27 +0100

    styled deleteButton

--- src/component/ColorCard/StyledDeleteButton.js
+++ src/component/ColorCard/StyledDeleteButton.js
@@ -2,8 +2,17 @@ import styled from "styled-components";
 
 const StyledDeleteButton = styled.button`
   position: absolute;
-  top: 0;
-  right: 0;
+  top: 5px;
+  right: 5px;
+  border: solid;
+  border-radius: 50%;
   cursor: pointer;
+  height: 30px;
+  width: 30px;
+  font-size: 18px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start; ;
 `;
 export default StyledDeleteButton;
