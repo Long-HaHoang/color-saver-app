2bcaa6e616dfd1834116c1db30a4c41566af5245
LongHaHoang <longhahoang1992@gmail.com>
2023-01-08 00:47:25 +0100

    fetch colorName from API update state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import useLocalStorageState from "use-local-storage-state";
+import useSWR from "swr";
 
 import { initialColors } from "./util/initialColors";
 import handleDelete from "./util/handleDelete";
@@ -7,31 +8,81 @@ import handleColorPick from "./util/handleColorCard";
 
 import ColorCard from "./component/ColorCard";
 import ColorPickerForm from "./component/ColorPickerForm";
+import { useEffect } from "react";
+import { useState } from "react";
 
 console.clear();
-
-const StyledApp = styled.div`
-  text-align: center;
-  display: flex;
-  padding: 20px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 30px;
-`;
-
-const StyledCardContainer = styled.div`
-  display: flex;
-  gap: 20px;
-  justify-content: flex-start;
-  align-items: center;
-  flex-wrap: wrap;
-`;
 
 function App() {
   const [colorsState, setColorsState] = useLocalStorageState("colorsState", {
     defaultValue: initialColors,
   });
+
+  useEffect(() => {
+    async function fetchColorData() {
+      try {
+        const response = await fetch(
+          "https://www.thecolorapi.com/id?hex=cccccc"
+        );
+
+        if (!response.ok) {
+          throw new Error("Aargh! Almost had it!");
+        }
+        const data = await response.json();
+
+        return data.results;
+      } catch (error) {
+        console.error("Error :", error.message);
+      }
+    }
+    fetchColorData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchEachColor() {
+      let colorNameList = [];
+
+      for (let i = 0; i < colorsState.length; i++) {
+        try {
+          const response = await fetch(
+            `https://www.thecolorapi.com/id?hex=${colorsState[
+              i
+            ].colorCode.slice(1)}`
+          );
+
+          if (!response.ok) {
+            throw new Error("Aargh! Almost had it!");
+          }
+          const data = await response.json();
+
+          colorNameList.push({ ...colorsState[i], colorName: data.name.value });
+        } catch (error) {
+          console.error("Error :", error.message);
+        }
+      }
+
+      // const newColorState = colorsState.map(async (color) => {
+      //   try {
+      //     const response = await fetch(
+      //       `https://www.thecolorapi.com/id?hex=${color.colorCode.slice(1)}`
+      //     );
+
+      //     if (!response.ok) {
+      //       throw new Error("Aargh! Almost had it!");
+      //     }
+      //     const data = await response.json();
+
+      //     colorNameList.push({ ...color, colorName: data.name.value });
+      //   } catch (error) {
+      //     console.error("Error :", error.message);
+      //   }
+      // });
+
+      console.log(colorNameList);
+      setColorsState(colorNameList);
+    }
+    fetchEachColor();
+  }, [colorsState, setColorsState]);
 
   function createNewCard(data) {
     setColorsState([
@@ -70,6 +121,7 @@ function App() {
               onHandleColorPick={handleColorPick}
               onHandleDelete={handleDelete}
               onHandleColorInput={handleInputEdit}
+              name={element.colorName}
             />
           );
         })}
@@ -77,5 +129,23 @@ function App() {
     </StyledApp>
   );
 }
+
+const StyledApp = styled.div`
+  text-align: center;
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
+
+const StyledCardContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+`;
 
 export default App;
---
+++ src/component/ColorCard/StyledName.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const StyledName = styled.p`
+  align-self: flex-start;
+`;
+export default StyledName;
