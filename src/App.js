a8dba7503b225ad70a438930417649c9c1d89ad1
LongHaHoang <longhahoang1992@gmail.com>
2023-01-08 00:59:43 +0100

    removed second useEffect and comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import useLocalStorageState from "use-local-storage-state";
-import useSWR from "swr";
 
 import { initialColors } from "./util/initialColors";
 import handleDelete from "./util/handleDelete";
@@ -9,7 +8,6 @@ import handleColorPick from "./util/handleColorCard";
 import ColorCard from "./component/ColorCard";
 import ColorPickerForm from "./component/ColorPickerForm";
 import { useEffect } from "react";
-import { useState } from "react";
 
 console.clear();
 
@@ -17,26 +15,6 @@ function App() {
   const [colorsState, setColorsState] = useLocalStorageState("colorsState", {
     defaultValue: initialColors,
   });
-
-  useEffect(() => {
-    async function fetchColorData() {
-      try {
-        const response = await fetch(
-          "https://www.thecolorapi.com/id?hex=cccccc"
-        );
-
-        if (!response.ok) {
-          throw new Error("Aargh! Almost had it!");
-        }
-        const data = await response.json();
-
-        return data.results;
-      } catch (error) {
-        console.error("Error :", error.message);
-      }
-    }
-    fetchColorData();
-  }, []);
 
   useEffect(() => {
     async function fetchEachColor() {
@@ -60,23 +38,6 @@ function App() {
           console.error("Error :", error.message);
         }
       }
-
-      // const newColorState = colorsState.map(async (color) => {
-      //   try {
-      //     const response = await fetch(
-      //       `https://www.thecolorapi.com/id?hex=${color.colorCode.slice(1)}`
-      //     );
-
-      //     if (!response.ok) {
-      //       throw new Error("Aargh! Almost had it!");
-      //     }
-      //     const data = await response.json();
-
-      //     colorNameList.push({ ...color, colorName: data.name.value });
-      //   } catch (error) {
-      //     console.error("Error :", error.message);
-      //   }
-      // });
 
       console.log(colorNameList);
       setColorsState(colorNameList);
